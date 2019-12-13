5d0af6d03d08a5676143fb72cffce8523cfd5e0c
mobapp15 <mobapp15@student.ehb.be>
2019-12-13 16:54:21 +0100

    Added all controllers and error messages

--- src/main/java/deloof/wouter/recap_jee/controllers/AddController.java
+++ src/main/java/deloof/wouter/recap_jee/controllers/AddController.java
@@ -4,6 +4,7 @@ import deloof.wouter.recap_jee.model.Product;
 import deloof.wouter.recap_jee.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +18,24 @@ public class AddController {
     @Autowired
     ProductDAO dao;
 
-    @ModelAttribute("nProduct")
+    @ModelAttribute("newProduct")
     public Product addNewProduct() {
         return new Product();
     }
 
+    //Ga naar âgina en maak een lege modelmap aan om de gegevens uit bovenstaande modelAttribute te halen
+    @RequestMapping( value = "/new", method = RequestMethod.GET)
+    public String showNew(ModelMap map) {
+        return "new";
+    }
+
     @RequestMapping( value = "/add", method = RequestMethod.POST)
-    public String showNew(@ModelAttribute(name = "nProduct") @Valid Product product, BindingResult br) {
+    public String saveNew(@ModelAttribute("newProduct") @Valid Product product, BindingResult br) {
+        System.out.println(br);
+        if(br.hasErrors()){
+            return "new";
+        }
         dao.save(product);
         return "redirect:/index";
     }
-
 }
--- src/main/java/deloof/wouter/recap_jee/controllers/IndexController.java
+++ src/main/java/deloof/wouter/recap_jee/controllers/IndexController.java
@@ -5,10 +5,13 @@ import deloof.wouter.recap_jee.model.ProductDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class IndexController {
@@ -25,12 +28,6 @@ public class IndexController {
     @RequestMapping( value = {"", "/", "/index"}, method = RequestMethod.GET)
     public String showIndex(ModelMap map){
         return "index";
-    }
-
-//    LINK TO NEW PAGE
-    @RequestMapping( value = "/new", method = RequestMethod.GET)
-    public String showNew(ModelMap map) {
-        return "new";
     }
 
 //    LINK TO DETAIL PAGE
